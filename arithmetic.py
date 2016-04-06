dfd83787f0648ef25d656caef1a58df15f358000
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:11:40 -0700

    Updated addition function

---
+++ arithmetic.py
@@ -0,0 +1,26 @@
+def add(num1, num2):
+    """ add two numbers and returns 
+    the sum
+    """
+    return num1 + num2
+
+def subtract(num1, num2):
+    pass
+
+def multiply(num1, num2):
+    pass
+
+def divide(num1, num2):
+    pass
+
+def square(num1):
+    pass
+
+def cube(num1):
+    pass
+
+def power(num1, num2):
+    pass
+
+def mod(num1, num2):
+    pass
